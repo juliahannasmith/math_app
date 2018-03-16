7a8e5eb378d100d58d004670150d675c156c7b8e
Julia H Smith <js44684@srcs.loc>
2018-03-16 11:27:18 -0700

    !

--- mathApp.js
+++ mathApp.js
@@ -2,6 +2,8 @@ var a = document.getElementById('a');
 var b = document.getElementById('b');
 var c = document.getElementById('c');
 var x = document.getelementById(   )
-function quadratic (){
+function solve(a,b,c){
+    var result1 = ((-1*b) + Math.sqrt((Math.pow(b,2)) - (4*a*c))/(2*a));
+    var result2 = ((-1*b) - Math.sqrt((Math.pow(b,2)) - (4*a*c))/(2*a));
 
 }
