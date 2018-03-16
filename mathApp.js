684b8ee2cbfb0a78947f1c0a239ff06c8e9c855d
Julia H Smith <js44684@srcs.loc>
2018-03-16 10:16:55 -0700

    started math app

---
+++ mathApp.js
@@ -0,0 +1,7 @@
+var a = document.getElementById('a');
+var b = document.getElementById('b');
+var c = document.getElementById('c');
+var x = document.getelementById(   )
+function quadratic (){
+
+}
