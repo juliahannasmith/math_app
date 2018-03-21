ec1827e9e97ac1e5b21499e30ca065539af55b65
Julia H Smith <js44684@srcs.loc>
2018-03-21 11:18:44 -0700

    solve

--- mathApp.js
+++ mathApp.js
@@ -1,9 +1,17 @@
-var a = document.getElementById('a');
-var b = document.getElementById('b');
-var c = document.getElementById('c');
-var x = document.getelementById(   )
-function solve(a,b,c){
-    var result1 = ((-1*b) + Math.sqrt((Math.pow(b,2)) - (4*a*c))/(2*a));
-    var result2 = ((-1*b) - Math.sqrt((Math.pow(b,2)) - (4*a*c))/(2*a));
+var button = document.getElementById('solve')
+function solve() {
+    var a = document.getElementById('a');
+    var b = document.getElementById('b');
+    var c = document.getElementById('c');
+    var result1 = ((-1 * b) + Math.sqrt((Math.pow(b, 2)) - (4 * a * c)) / (2 * a));
+    var result2 = ((-1 * b) - Math.sqrt((Math.pow(b, 2)) - (4 * a * c)) / (2 * a));
 
 }
+
+function displayAnswer(){
+    var answer = result1 + result2
+    return answer
+}
+
+button.addEventListener('click', displayAnswer)
+
