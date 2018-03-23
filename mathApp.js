a3e67cfa372fcea14ea54229c521c04c043c5f4a
Julia H Smith <js44684@srcs.loc>
2018-03-23 12:19:51 -0700

    i tried

--- mathApp.js
+++ mathApp.js
@@ -1,17 +1,23 @@
 var button = document.getElementById('solve')
+
 function solve() {
-    var a = document.getElementById('a');
-    var b = document.getElementById('b');
-    var c = document.getElementById('c');
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;
+    var c = document.getElementById('c').value;
     var result1 = ((-1 * b) + Math.sqrt((Math.pow(b, 2)) - (4 * a * c)) / (2 * a));
     var result2 = ((-1 * b) - Math.sqrt((Math.pow(b, 2)) - (4 * a * c)) / (2 * a));
 
+    if (isNaN(a)||isNaN(b)||isNaN(c) == 'true') {
+        return "You entered an incorrect value for either A, B, or C. Please try again.";
+    } else {
+        return "The roots to the quadratic are " + result1 + " and " + result2 + ".";
+    }
 }
 
-function displayAnswer(){
-    var answer = result1 + result2
-    return answer
+
+function displayAnswer() {
+    var result = solve();
+    return document.getElementById('solutions').textContent = result;
 }
 
 button.addEventListener('click', displayAnswer)
-
