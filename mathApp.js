8aec6af6a115ddd489964c0b12afbb1ae902c334
Julia Smith <juliasmith@Julias-Air.hsd1.ca.comcast.net>
2018-03-25 19:26:53 -0700

    done, check some things

--- mathApp.js
+++ mathApp.js
@@ -1,15 +1,25 @@
-var button = document.getElementById('solve')
+var button = document.getElementById('solve');
+
+function changeElementClass(id, className){
+    var el = document.getElementById(id);
+    el.className = className;
+}
 
 function solve() {
     var a = document.getElementById('a').value;
     var b = document.getElementById('b').value;
     var c = document.getElementById('c').value;
-    var result1 = ((-1 * b) + Math.sqrt((Math.pow(b, 2)) - (4 * a * c)) / (2 * a));
-    var result2 = ((-1 * b) - Math.sqrt((Math.pow(b, 2)) - (4 * a * c)) / (2 * a));
+    var result1 = (((-1 * b) + Math.sqrt((Math.pow(b, 2)) - (4 * a * c))) / (2 * a));
+    var result2 = (((-1 * b) - Math.sqrt((Math.pow(b, 2)) - (4 * a * c))) / (2 * a));
 
-    if (isNaN(a)||isNaN(b)||isNaN(c) == 'true') {
+    if (isNaN(a)||isNaN(b)||isNaN(c) == 'true' || a==0) {
+        changeElementClass("solutions", "invalid");
         return "You entered an incorrect value for either A, B, or C. Please try again.";
+    } else if (((Math.pow(b, 2)) - (4 * a * c)) < 0) {
+        changeElementClass("solutions", "noreal");
+        return "There are no real solutions."
     } else {
+        changeElementClass("solutions", "real");
         return "The roots to the quadratic are " + result1 + " and " + result2 + ".";
     }
 }
@@ -20,4 +30,4 @@ function displayAnswer() {
     return document.getElementById('solutions').textContent = result;
 }
 
-button.addEventListener('click', displayAnswer)
+button.addEventListener('click', displayAnswer);
